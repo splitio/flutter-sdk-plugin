fe1d9324dcf59d04c4b893080a45d1ca2b75e6c0
Gaston Thea <gaston.thea@split.io>
2022-06-06 17:09:18 -0300

    Handle ClassCastException

--- android/src/main/java/io/split/splitio/ArgumentParserImpl.java
+++ android/src/main/java/io/split/splitio/ArgumentParserImpl.java
@@ -6,25 +6,39 @@ import androidx.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.split.android.client.utils.Logger;
+
 class ArgumentParserImpl implements ArgumentParser {
 
     @Nullable
     @Override
     public String getStringArgument(@NonNull String argumentName, @NonNull Object arguments) {
-        Map<String, Object> argMap = (Map<String, Object>) arguments;
-        return (String) argMap.get(argumentName);
+        try {
+            Map<String, Object> argMap = (Map<String, Object>) arguments;
+            return (String) argMap.get(argumentName);
+        } catch (ClassCastException exception) {
+            return null;
+        }
     }
 
     @NonNull
     @Override
     public Map<String, Object> getMapArgument(@NonNull String argumentName, @NonNull Object arguments) {
-        Map<String, Object> argMap = (Map<String, Object>) arguments;
-        return (argMap.get(argumentName) == null) ? new HashMap<>() : (Map<String, Object>) argMap.get(argumentName);
+        try {
+            Map<String, Object> argMap = (Map<String, Object>) arguments;
+            return (argMap.get(argumentName) == null) ? new HashMap<>() : (Map<String, Object>) argMap.get(argumentName);
+        } catch (ClassCastException exception) {
+            return new HashMap<>();
+        }
     }
 
     @Override
     public boolean getBooleanArgument(@NonNull String argumentName, @NonNull Object arguments) {
-        Map<String, Object> argMap = (Map<String, Object>) arguments;
-        return (argMap.get(argumentName) == null) ? false : (boolean) argMap.get(argumentName);
+        try {
+            Map<String, Object> argMap = (Map<String, Object>) arguments;
+            return (argMap.get(argumentName) == null) ? false : (boolean) argMap.get(argumentName);
+        } catch (ClassCastException exception) {
+            return false;
+        }
     }
 }
--- android/src/test/java/io/split/splitio/ArgumentParserImplTest.java
+++ android/src/test/java/io/split/splitio/ArgumentParserImplTest.java
@@ -2,12 +2,14 @@ package io.split.splitio;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +34,15 @@ public class ArgumentParserImplTest {
     }
 
     @Test
+    public void testGetStringArgumentFailure() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("matchingKey", false);
+        String matchingKey = mArgumentParser.getStringArgument("matchingKey", map);
+
+        assertNull(matchingKey);
+    }
+
+    @Test
     public void testGetMapArgument() {
         Map<String, Object> map = new HashMap<>();
         map.put("apiKey", "api-key");
@@ -45,6 +56,15 @@ public class ArgumentParserImplTest {
     }
 
     @Test
+    public void testGetMapArgumentFailure() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("apiKey", "api-key");
+        Map<String, Object> mapArgument = mArgumentParser.getMapArgument("mapArgument", map);
+
+        assertEquals(Collections.emptyMap(), mapArgument);
+    }
+
+    @Test
     public void testGetBooleanArgument() {
         Map<String, Object> arguments = new HashMap<>();
         arguments.put("trueArgument", true);
@@ -54,5 +74,15 @@ public class ArgumentParserImplTest {
 
         assertTrue(trueArgument);
         assertFalse(falseArgument);
+    }
+
+    @Test
+    public void testGetBooleanFailure() {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("trueArgument", "true");
+
+        boolean trueArgument = mArgumentParser.getBooleanArgument("trueArgument", arguments);
+
+        assertFalse(trueArgument);
     }
 }
