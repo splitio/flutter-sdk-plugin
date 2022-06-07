24776a29531cfeb3109c6f9ce152aefa78ef1976
Gaston Thea <gaston.thea@split.io>
2022-06-07 16:24:36 -0300

    WIP

---
+++ ios/Classes/SplitClientConfigHelper.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Split
+
+class SplitClientConfigHelper {
+
+    private let FEATURES_REFRESH_RATE = "featuresRefreshRate";
+    private let SEGMENTS_REFRESH_RATE = "segmentsRefreshRate";
+    private let IMPRESSIONS_REFRESH_RATE = "impressionsRefreshRate";
+    private let TELEMETRY_REFRESH_RATE = "telemetryRefreshRate";
+    private let EVENTS_QUEUE_SIZE = "eventsQueueSize";
+    private let IMPRESSIONS_QUEUE_SIZE = "impressionsQueueSize";
+    private let EVENT_FLUSH_INTERVAL = "eventFlushInterval";
+    private let EVENTS_PER_PUSH = "eventsPerPush";
+    private let TRAFFIC_TYPE = "trafficType";
+    private let CONNECTION_TIME_OUT = "connectionTimeOut";
+    private let READ_TIMEOUT = "readTimeout";
+    private let DISABLE_LABELS = "disableLabels";
+    private let ENABLE_DEBUG = "enableDebug";
+    private let PROXY_HOST = "proxyHost";
+    private let READY = "ready";
+    private let STREAMING_ENABLED = "streamingEnabled";
+    private let PERSISTENT_ATTRIBUTES_ENABLED = "persistentAttributesEnabled";
+    private let API_ENDPOINT = "apiEndpoint";
+    private let EVENTS_ENDPOINT = "eventsEndpoint";
+    private let SSE_AUTH_SERVICE_ENDPOINT = "sseAuthServiceEndpoint";
+    private let STREAMING_SERVICE_ENDPOINT = "streamingServiceEndpoint";
+    private let TELEMETRY_SERVICE_ENDPOINT = "telemetryServiceEndpoint";
+    
+    func fromMap(configurationMap: [String: Any?]) {
+        return SplitClientConfig()
+    }
+}
--- ios/Classes/SplitMethodParser.swift
+++ ios/Classes/SplitMethodParser.swift
@@ -1,8 +1,55 @@
-//
-//  SplitMethodParser.swift
-//  SplitMethodParser
-//
-//  Created by Gaston Thea on 07/06/2022.
-//
-
 import Foundation
+
+protocol SplitMethodParser {
+    
+    func onMethodCall(methodName: String, arguments: Any, result: FlutterResult)
+}
+
+class DefaultSplitMethodParser : SplitMethodParser {
+    
+    private var splitWrapper: SplitWrapper?
+    private let argumentParser: ArgumentParser
+    
+    private let METHOD_INIT = "init";
+    private let METHOD_GET_CLIENT = "getClient";
+    private let METHOD_DESTROY = "destroy";
+
+    private let ARG_API_KEY = "apiKey";
+    private let ARG_MATCHING_KEY = "matchingKey";
+    private let ARG_BUCKETING_KEY = "bucketingKey";
+    private let ARG_CONFIG = "sdkConfiguration";
+    private let ARG_WAIT_FOR_READY = "waitForReady";
+    private let ERROR_SDK_NOT_INITIALIZED = "SDK_NOT_INITIALIZED";
+    private let ERROR_SDK_NOT_INITIALIZED_MESSAGE = "Split SDK has not been initialized";
+    
+    init() {
+        argumentParser = DefaultArgumentParser()
+    }
+
+    init(splitWrapper: SplitWrapper, argumentParser: ArgumentParser) {
+        self.splitWrapper = splitWrapper
+        self.argumentParser = argumentParser
+    }
+
+    func onMethodCall(methodName: String, arguments: Any, result: (Any?) -> Void) {
+        switch (methodName) {
+            case METHOD_INIT:
+                initializeSplit(
+                    apiKey: argumentParser.getStringArgument(argumentName: ARG_API_KEY, arguments: arguments) ?? "",
+                    matchingKey: argumentParser.getStringArgument(argumentName: ARG_MATCHING_KEY, arguments: arguments) ?? "",
+                    bucketingKey: argumentParser.getStringArgument(argumentName: ARG_BUCKETING_KEY, arguments: arguments),
+                    configurationMap: argumentParser.getMapArgument(argumentName: ARG_CONFIG, arguments: arguments),
+                    result: result
+                )
+                break
+            else:
+                break
+        }
+    }
+    
+    private func initializeSplit(apiKey: String, matchingKey: String, bucketingKey: String?, configurationMap [String, Any?]?, result: FlutterResult) {
+        splitWrapper = SplitWrapper(SplitFactoryProvider:
+            SplitFactoryProvider(apiKey, matchingKey, bucketingKey, SplitClientConfigHelper.fromMap(mapArgument))
+        )
+    }
+}
