22443de1235b2e5f811e43aa5ca1a7118392f3a4
Gastón Thea <gaston.thea@split.io>
2022-06-15 14:34:13 -0300

    Dart configuration object (#9)

--- lib/splitio.dart
+++ lib/splitio.dart
@@ -1,7 +1,7 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:splitio/split_configuration.dart';
 
 class Splitio {
   static const MethodChannel _channel = MethodChannel('splitio');
@@ -9,5 +9,15 @@ class Splitio {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  Future<void> initializeSplit(String apiKey, String matchingKey,
+      [String? bucketingKey, SplitConfiguration? configuration]) async {
+    await _channel.invokeMethod('init', {
+      'apiKey': apiKey,
+      'matchingKey': matchingKey,
+      'bucketingKey': bucketingKey,
+      'sdkConfiguration': configuration?.configurationMap ?? {},
+    });
   }
 }
