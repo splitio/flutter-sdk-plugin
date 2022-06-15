ef92e457abb39cbbb2053f63dd1d8b9267688db5
Gaston Thea <gaston.thea@split.io>
2022-06-15 15:20:49 -0300

    Remove nullability in split view properties

--- lib/split_view.dart
+++ lib/split_view.dart
@@ -1,18 +1,13 @@
 import 'dart:core';
 
 class SplitView {
-  String? name;
-  String? trafficType;
-  bool? killed = false;
-  List<String>? treatments = [];
-  int? changeNumber;
-  Map<String, String>? configs = {};
+  String name;
+  String trafficType;
+  bool killed = false;
+  List<String> treatments = [];
+  int changeNumber;
+  Map<String, String> configs = {};
 
-  SplitView(
-      {this.name,
-      this.trafficType,
-      this.killed,
-      this.treatments,
-      this.changeNumber,
-      this.configs});
+  SplitView(this.name, this.trafficType, this.killed, this.treatments,
+      this.changeNumber, this.configs);
 }
