6ccab01e23a2c01e7b491816582128f4a0a85bbd
Gaston Thea <gaston.thea@split.io>
2022-06-24 13:51:27 -0300

    Various fixes

--- ios/Classes/Constants.swift
+++ ios/Classes/Constants.swift
@@ -1,19 +1,17 @@
 import Foundation
 
-struct Constants {
 
-    struct Methods {
-        static let METHOD_INIT = "init"
-        static let METHOD_GET_CLIENT = "getClient"
-        static let METHOD_DESTROY = "destroy"
-        static let METHOD_CLIENT_READY = "clientReady"
-    }
+enum Method : String {
+    case initialize = "init"
+    case client = "getClient"
+    case destroy = "destroy"
+    case clientReady = "clientReady"
+}
 
-    struct Arguments {
-        static let ARG_API_KEY = "apiKey"
-        static let ARG_MATCHING_KEY = "matchingKey"
-        static let ARG_BUCKETING_KEY = "bucketingKey"
-        static let ARG_CONFIG = "sdkConfiguration"
-        static let ARG_WAIT_FOR_READY = "waitForReady"
-    }
+enum Argument : String {
+    case apiKey = "apiKey"
+    case matchingKey = "matchingKey"
+    case bucketingKey = "bucketingKey"
+    case config = "config"
+    case waitForReady = "waitForReady"
 }
