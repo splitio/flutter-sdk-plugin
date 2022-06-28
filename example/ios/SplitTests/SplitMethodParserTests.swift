b2717d3361f39d454f4cff0338cd5060c1c0b0ee
Gaston Thea <gaston.thea@split.io>
2022-06-28 16:37:07 -0300

    iOS method parser evaluation tests

--- example/ios/SplitTests/SplitMethodParserTests.swift
+++ example/ios/SplitTests/SplitMethodParserTests.swift
@@ -17,7 +17,6 @@ class SplitMethodParserTests: XCTestCase {
     }
 
     func testSuccessfulGetClient() throws {
-
         methodParser?.onMethodCall(
             methodName: "getClient",
             arguments: ["matchingKey": "user-key", "bucketingKey": "bucketing-key", "waitForReady": true],
@@ -29,6 +28,59 @@ class SplitMethodParserTests: XCTestCase {
         if let wrapper = (splitWrapper as? SplitWrapperStub) {
             XCTAssert(wrapper.matchingKeyValue == "user-key")
             XCTAssert(wrapper.bucketingKeyValue == "bucketing-key")
+        }
+    }
+
+    func testGetTreatment() {
+        methodParser?.onMethodCall(methodName: "getTreatment", arguments: ["matchingKey": "user-key", "bucketingKey": "bucketing-key", "splitName": "split1", "attributes": ["age": 50]], result: { (_: Any?) in
+            return
+        })
+
+        if let splitWrapper = (splitWrapper as? SplitWrapperStub) {
+            XCTAssert(splitWrapper.matchingKeyValue == "user-key")
+            XCTAssert(splitWrapper.bucketingKeyValue == "bucketing-key")
+            XCTAssert(splitWrapper.splitNameValue == "split1")
+            XCTAssert(NSDictionary(dictionary: ["age": 50]).isEqual(to: splitWrapper.attributesValue!))
+        }
+    }
+
+    func testGetTreatmentWithConfig() {
+        methodParser?.onMethodCall(methodName: "getTreatmentWithConfig", arguments: ["matchingKey": "user-key", "bucketingKey": "bucketing-key", "splitName": "split1", "attributes": ["age": 50]], result: { (_: Any?) in
+            return
+        })
+
+        if let splitWrapper = (splitWrapper as? SplitWrapperStub) {
+            XCTAssert(splitWrapper.matchingKeyValue == "user-key")
+            XCTAssert(splitWrapper.bucketingKeyValue == "bucketing-key")
+            XCTAssert(splitWrapper.splitNameValue == "split1")
+            XCTAssert(splitWrapper.attributesValue != nil)
+            XCTAssert(NSDictionary(dictionary: ["age": 50]).isEqual(to: splitWrapper.attributesValue!))
+        }
+    }
+
+    func testGetTreatments() {
+        methodParser?.onMethodCall(methodName: "getTreatments", arguments: ["matchingKey": "user-key", "bucketingKey": "bucketing-key", "splitName": ["split1"], "attributes": ["age": 50]], result: { (_: Any?) in
+            return
+        })
+
+        if let splitWrapper = (splitWrapper as? SplitWrapperStub) {
+            XCTAssert(splitWrapper.matchingKeyValue == "user-key")
+            XCTAssert(splitWrapper.bucketingKeyValue == "bucketing-key")
+            XCTAssert(splitWrapper.splitsValue == ["split1"])
+            XCTAssert(NSDictionary(dictionary: ["age": 50]).isEqual(to: splitWrapper.attributesValue!))
+        }
+    }
+
+    func testGetTreatmentsWithConfig() {
+        methodParser?.onMethodCall(methodName: "getTreatmentsWithConfig", arguments: ["matchingKey": "user-key", "bucketingKey": "bucketing-key", "splitName": ["split1"], "attributes": ["age": 50]], result: { (_: Any?) in
+            return
+        })
+
+        if let splitWrapper = (splitWrapper as? SplitWrapperStub) {
+            XCTAssert(splitWrapper.matchingKeyValue == "user-key")
+            XCTAssert(splitWrapper.bucketingKeyValue == "bucketing-key")
+            XCTAssert(splitWrapper.splitsValue == ["split1"])
+            XCTAssert(NSDictionary(dictionary: ["age": 50]).isEqual(to: splitWrapper.attributesValue!))
         }
     }
 
@@ -54,7 +106,10 @@ class SplitWrapperStub: SplitWrapper {
 
     var destroyCalled = false
     var matchingKeyValue = ""
-    var bucketingKeyValue = ""
+    var bucketingKeyValue: String?
+    var splitNameValue = ""
+    var splitsValue: [String]?
+    var attributesValue: [String: Any]?
 
     func getClient(matchingKey: String, bucketingKey: String?) -> SplitClient? {
         matchingKeyValue = matchingKey
@@ -64,10 +119,18 @@ class SplitWrapperStub: SplitWrapper {
     }
 
     func getTreatment(matchingKey: String, splitName: String, bucketingKey: String?, attributes: [String: Any]?) -> String? {
+        matchingKeyValue = matchingKey
+        bucketingKeyValue = bucketingKey
+        splitNameValue = splitName
+        attributesValue = attributes
         return "control"
     }
 
     func getTreatments(matchingKey: String, splits: [String], bucketingKey: String?, attributes: [String: Any]?) -> [String: String] {
+        matchingKeyValue = matchingKey
+        bucketingKeyValue = bucketingKey
+        splitsValue = splits
+        attributesValue = attributes
         var result: [String: String] = [:]
         splits.forEach {
             result[$0] = "control"
@@ -77,10 +140,18 @@ class SplitWrapperStub: SplitWrapper {
     }
 
     func getTreatmentWithConfig(matchingKey: String, splitName: String, bucketingKey: String?, attributes: [String: Any]?) -> SplitResult? {
+        matchingKeyValue = matchingKey
+        bucketingKeyValue = bucketingKey
+        splitNameValue = splitName
+        attributesValue = attributes
         return SplitResult(treatment: "control", config: nil)
     }
 
     func getTreatmentsWithConfig(matchingKey: String, splits: [String], bucketingKey: String?, attributes: [String: Any]?) -> [String: SplitResult] {
+        matchingKeyValue = matchingKey
+        bucketingKeyValue = bucketingKey
+        splitsValue = splits
+        attributesValue = attributes
         var result: [String: SplitResult] = [:]
         splits.forEach {
             result[$0] = SplitResult(treatment: "control", config: nil)
