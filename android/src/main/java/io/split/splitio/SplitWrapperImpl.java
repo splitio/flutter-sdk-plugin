71d52f999ca721e0f9b1f4bdad70f040c1eed47b
Gastón Thea <gaston.thea@split.io>
2022-07-25 11:14:05 -0300

    Getter for initialized client (#23)

--- android/src/main/java/io/split/splitio/SplitWrapperImpl.java
+++ android/src/main/java/io/split/splitio/SplitWrapperImpl.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,6 +15,7 @@ import io.split.android.client.SplitFactory;
 import io.split.android.client.SplitResult;
 import io.split.android.client.api.Key;
 import io.split.android.client.utils.ConcurrentSet;
+import io.split.android.grammar.Treatments;
 
 class SplitWrapperImpl implements SplitWrapper {
 
@@ -39,9 +42,24 @@ class SplitWrapperImpl implements SplitWrapper {
         return client;
     }
 
+    @Nullable
+    private SplitClient getInitializedClient(String matchingKey, @Nullable String bucketingKey) {
+        Key key = Helper.buildKey(matchingKey, bucketingKey);
+        if (mUsedKeys.contains(key)) {
+            return mSplitFactory.client(matchingKey, bucketingKey);
+        }
+
+        return null;
+    }
+
     @Override
     public boolean track(String matchingKey, @Nullable String bucketingKey, String eventType, @Nullable String trafficType, @Nullable Double value, Map<String, Object> properties) {
-        SplitClient client = getClient(matchingKey, bucketingKey);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+
+        if (client == null) {
+            return false;
+        }
+
         if (trafficType != null) {
             if (value != null) {
                 return client.track(trafficType, eventType, value, properties);
@@ -59,70 +77,131 @@ class SplitWrapperImpl implements SplitWrapper {
 
     @Override
     public String getTreatment(String matchingKey, String bucketingKey, String splitName, Map<String, Object> attributes) {
-        return getClient(matchingKey, bucketingKey).getTreatment(splitName, attributes);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return Treatments.CONTROL;
+        }
+
+        return client.getTreatment(splitName, attributes);
     }
 
     @Override
     public Map<String, String> getTreatments(String matchingKey, String bucketingKey, List<String> splitNames, Map<String, Object> attributes) {
-        return getClient(matchingKey, bucketingKey).getTreatments(splitNames, attributes);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            Map<String, String> defaultResult = new HashMap<>();
+            for (String splitName: splitNames) {
+                defaultResult.put(splitName, Treatments.CONTROL);
+            }
+
+            return defaultResult;
+        }
+
+        return client.getTreatments(splitNames, attributes);
     }
 
     @Override
     public SplitResult getTreatmentWithConfig(String matchingKey, String bucketingKey, String splitName, Map<String, Object> attributes) {
-        return getClient(matchingKey, bucketingKey).getTreatmentWithConfig(splitName, attributes);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return new SplitResult(Treatments.CONTROL);
+        }
+
+        return client.getTreatmentWithConfig(splitName, attributes);
     }
 
     @Override
     public Map<String, SplitResult> getTreatmentsWithConfig(String matchingKey, String bucketingKey, List<String> splitNames, Map<String, Object> attributes) {
-        return getClient(matchingKey, bucketingKey).getTreatmentsWithConfig(splitNames, attributes);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            Map<String, SplitResult> defaultResult = new HashMap<>();
+            for (String splitName: splitNames) {
+                defaultResult.put(splitName, new SplitResult(Treatments.CONTROL));
+            }
+
+            return defaultResult;
+        }
+
+        return client.getTreatmentsWithConfig(splitNames, attributes);
     }
 
     @Override
     public boolean setAttribute(String matchingKey, @Nullable String bucketingKey, String attributeName, Object value) {
-        return getClient(matchingKey, bucketingKey).setAttribute(attributeName, value);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return false;
+        }
+
+        return client.setAttribute(attributeName, value);
     }
 
     @Nullable
     @Override
     public Object getAttribute(String matchingKey, @Nullable String bucketingKey, String attributeName) {
-        return getClient(matchingKey, bucketingKey).getAttribute(attributeName);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return null;
+        }
+
+        return client.getAttribute(attributeName);
     }
 
     @Override
     public boolean setAttributes(String matchingKey, @Nullable String bucketingKey, Map<String, Object> attributes) {
-        return getClient(matchingKey, bucketingKey).setAttributes(attributes);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return false;
+        }
+
+        return client.setAttributes(attributes);
     }
 
     @NonNull
     @Override
     public Map<String, Object> getAllAttributes(String matchingKey, @Nullable String bucketingKey) {
-        return getClient(matchingKey, bucketingKey).getAllAttributes();
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return Collections.emptyMap();
+        }
+
+        return client.getAllAttributes();
     }
 
     @Override
     public boolean removeAttribute(String matchingKey, @Nullable String bucketingKey, String attributeName) {
-        return getClient(matchingKey, bucketingKey).removeAttribute(attributeName);
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return false;
+        }
+
+        return client.removeAttribute(attributeName);
     }
 
     @Override
     public boolean clearAttributes(String matchingKey, @Nullable String bucketingKey) {
-        return getClient(matchingKey, bucketingKey).clearAttributes();
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client == null) {
+            return false;
+        }
+
+        return client.clearAttributes();
     }
 
     @Override
     public void flush(String matchingKey, @Nullable String bucketingKey) {
-        Key requestedKey = new Key(matchingKey, bucketingKey);
-        if (mUsedKeys.contains(requestedKey)) {
-            getClient(matchingKey, bucketingKey).flush();
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client != null) {
+            client.flush();
         }
     }
 
     @Override
     public void destroy(String matchingKey, @Nullable String bucketingKey) {
         Key requestedKey = new Key(matchingKey, bucketingKey);
-        if (mUsedKeys.contains(requestedKey)) {
-            getClient(matchingKey, bucketingKey).destroy();
 
+        SplitClient client = getInitializedClient(matchingKey, bucketingKey);
+        if (client != null) {
+            client.destroy();
             mUsedKeys.remove(requestedKey);
         }
     }
