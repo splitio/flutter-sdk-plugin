e06e0f8a4a272b9e0f941a94aef985d448442936
Gaston Thea <gaston.thea@split.io>
2022-09-07 16:52:18 -0300

    Add default tests for iOS platform

--- splitio_ios/lib/splitio_ios.dart
+++ splitio_ios/lib/splitio_ios.dart
@@ -4,7 +4,7 @@ import 'package:splitio_platform_interface/splitio_platform_interface.dart';
 const MethodChannel _methodChannel = MethodChannel('split.io/splitio_ios');
 
 /// Implementation for iOS of [SplitioPlatform].
-class SplitioIos extends MethodChannelPlatform {
+class SplitioIOS extends MethodChannelPlatform {
   /// Registers this class as the default platform implementation.
   static void registerWith() {
     SplitioPlatform.instance = SplitioIOS();
