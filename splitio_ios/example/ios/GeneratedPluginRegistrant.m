231826111b36ea682202f36ad3b10209f6470563
Gaston Thea <gaston.thea@split.io>
2022-09-08 16:04:14 -0300

    Example in splitio_ios

---
+++ splitio_ios/example/ios/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<splitio_ios/SwiftSplitioPlugin.h>)
+#import <splitio_ios/SwiftSplitioPlugin.h>
+#else
+@import splitio_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SwiftSplitioPlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftSplitioPlugin"]];
+}
+
+@end
