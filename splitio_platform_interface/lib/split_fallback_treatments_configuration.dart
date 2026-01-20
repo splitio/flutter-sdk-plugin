0deae7f597ed1eccd1646cede0f3f74c17e31f0d
Emiliano Sanchez <emiliano.sanchez@split.io>
2026-01-20 11:48:34 -0300

    Polishing

--- splitio_platform_interface/lib/split_fallback_treatments_configuration.dart
+++ splitio_platform_interface/lib/split_fallback_treatments_configuration.dart
@@ -1,8 +1,8 @@
 import 'package:splitio_platform_interface/split_result.dart';
 
 class FallbackTreatmentsConfiguration {
-  late Map<String, String?>? _global;
-  late Map<String, Map<String, String?>>? _byFlag;
+  Map<String, String?>? _global;
+  Map<String, Map<String, String?>>? _byFlag;
 
   Map<String, String?>? get global => _global;
   Map<String, Map<String, String?>>? get byFlag => _byFlag;
